bbf8f5a8a5e3c26e2190f337df2dfbf16341d448
satk78204-crypto <satk78204@gmail.com>
2026-01-08 20:23:32 +0530

    update

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ function downloadPDF() {
     margin:       0.3,
     filename:     `Bonafide_${regNoField.innerText.trim()}.pdf`,
     image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 2 },
+    html2canvas:  { scale: 2 , useCORS: true,scrollY: 0},
     jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
   };
   html2pdf().set(opt).from(element).save();
